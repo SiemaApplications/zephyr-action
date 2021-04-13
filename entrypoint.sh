cce4f3878e30154b803b71f80906bd6520457ed2
Nicolas VINCENT <nicolas.vincent@vossloh.com>
2021-04-13 16:22:49 +0200

    entypoint handles twister
    docker entrypoint handles twister build.
    board and app dir are passed as env variable
    first draft at action.yml
    
    Signed-off-by: Nicolas VINCENT <nicolas.vincent@vossloh.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
 MANIFESTDIR=${INPUT_MANIFESTDIR:?INPUT_MANIFESTDIR must be specified}
 INIT=${INPUT_INIT:-false}
 UPDATE=${INPUT_UPDATE:-false}
+TWISTER=${INPUT_TWISTER:-false}
+
 
 if [ "${INIT}" = "true" ]; then
     west init -l ${MANIFESTDIR}
@@ -9,4 +11,21 @@ fi
 
 if [ "${UPDATE}" = "true" ]; then
     west update
+fi
+
+if [ "${TWISTER}" = "true" ]; then
+    TWISTER_BOARD=${INPUT_TWISTER_BOARD:?Missing board target name}
+    TWISTER_APP_DIR=${INPUT_TWISTER_APP_DIR:?Missing application directory}
+    if [ ! -z "${INPUT_TWISTER_BOARD_ROOT}" ]; then
+        if [ -d ${INPUT_TWISTER_BOARD_ROOT} ]; then
+            TWISTER_BOARD_ROOT_ARG="-A ${INPUT_TWISTER_BOARD_ROOT}"
+        else
+            echo "Error: INPUT_TWISTER_BOARD_ROOT: invalid directory ${INPUT_TWISTER_BOARD_ROOT}"
+            exit 1
+        fi
+    fi
+    set -x
+    ./zephyr/scripts/twister -p ${TWISTER_BOARD} ${TWISTER_BOARD_ROOT_ARG} \
+        -T ${TWISTER_APP_DIR}
+
 fi
