2d9f0b51916297cd39d369ccfa5961ab3f567a33
Nicolas VINCENT <nicolas.vincent@vossloh.com>
2021-08-19 15:14:12 +0200

    quotes variables
    Use quotes on assignement to have an extra layer of protection
    
    Signed-off-by: Nicolas VINCENT <nicolas.vincent@vossloh.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/sh -e
 MANIFESTDIR=${INPUT_MANIFESTDIR:?INPUT_MANIFESTDIR must be specified}
 INIT=${INPUT_INIT:-false}
 UPDATE=${INPUT_UPDATE:-false}
@@ -11,20 +11,21 @@ if [ "${INIT}" = "true" ]; then
 fi
 
 if [ "${UPDATE}" = "true" ]; then
-    west update ${INPUT_UPDATE_EXTRA_ARGS}
+    UPDATE_EXTRA_ARGS=${INPUT_UPDATE_EXTRA_ARGS}
+    west update ${UPDATE_EXTRA_ARGS}
 fi
 
 if [ "${BUILD}" = "true" ]; then
-    BUILD_BOARD=${INPUT_BUILD_BOARD:?Missing board target name}
-    BUILD_APP_DIR=${INPUT_BUILD_APP_DIR:?Missing application directory}
-    BUILD_DIR=${INPUT_BUILD_DIR:-build/${BUILD_BOARD}/$(basename ${BUILD_APP_DIR})}
-    CMAKE_EXTRA_ARGS=${INPUT_CMAKE_EXTRA_ARGS}
-    west build -p auto -b ${BUILD_BOARD} -d ${BUILD_DIR} ${BUILD_APP_DIR} -- ${CMAKE_EXTRA_ARGS}
+    BUILD_BOARD="${INPUT_BUILD_BOARD:?Missing board target name}"
+    BUILD_APP_DIR="${INPUT_BUILD_APP_DIR:?Missing application directory}"
+    BUILD_DIR="${INPUT_BUILD_DIR:-build/${BUILD_BOARD}/$(basename ${BUILD_APP_DIR})}"
+    CMAKE_EXTRA_ARGS="${INPUT_CMAKE_EXTRA_ARGS}"
+    west build -p auto -b "${BUILD_BOARD}" -d "${BUILD_DIR}" "${BUILD_APP_DIR}" -- ${CMAKE_EXTRA_ARGS}
 fi
 
 if [ "${TWISTER}" = "true" ]; then
-    TWISTER_BOARD=${INPUT_TWISTER_BOARD:?Missing board target name}
-    TWISTER_APP_DIR=${INPUT_TWISTER_APP_DIR:?Missing application directory}
+    TWISTER_BOARD="${INPUT_TWISTER_BOARD:?Missing board target name}"
+    TWISTER_APP_DIR="${INPUT_TWISTER_APP_DIR:?Missing application directory}"
     if [ ! -z "${INPUT_TWISTER_BOARD_ROOT}" ]; then
         if [ -d ${INPUT_TWISTER_BOARD_ROOT} ]; then
             TWISTER_BOARD_ROOT_ARG="-A ${INPUT_TWISTER_BOARD_ROOT}"
@@ -33,7 +34,7 @@ if [ "${TWISTER}" = "true" ]; then
             exit 1
         fi
     fi
-    ./zephyr/scripts/twister -p ${TWISTER_BOARD} ${TWISTER_BOARD_ROOT_ARG} \
-        -T ${TWISTER_APP_DIR}
+    ./zephyr/scripts/twister -p "${TWISTER_BOARD}" ${TWISTER_BOARD_ROOT_ARG} \
+        -T "${TWISTER_APP_DIR}"
 
 fi
