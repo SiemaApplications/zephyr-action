8652e084c58abc61adc4a25df3dcf80edcb5c1cf
Nicolas VINCENT <nicolas.vincent@vossloh.com>
2021-09-14 14:28:13 +0200

    Adds signing capability to zephyr-action.
    list of application to sign is passed in the form of a file containing
    the path and the version.
    
    Signed-off-by: Nicolas VINCENT <nicolas.vincent@vossloh.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ INIT="${INPUT_INIT:-false}"
 UPDATE="${INPUT_UPDATE:-false}"
 TWISTER="${INPUT_TWISTER:-false}"
 BUILD="${INPUT_BUILD:-false}"
+SIGN="${INPUT_SIGN:-false}"
 
 
 if [ "${INIT}" = "true" ]; then
@@ -20,7 +21,9 @@ if [ "${BUILD}" = "true" ]; then
     BUILD_APP_DIR="${INPUT_BUILD_APP_DIR:?Missing application directory}"
     BUILD_DIR="${INPUT_BUILD_DIR:-build/${BUILD_BOARD}/$(basename ${BUILD_APP_DIR})}"
     CMAKE_EXTRA_ARGS="${INPUT_CMAKE_EXTRA_ARGS}"
-    west build -p auto -b "${BUILD_BOARD}" -d "${BUILD_DIR}" "${BUILD_APP_DIR}" -- ${CMAKE_EXTRA_ARGS}
+    west build -p auto -b "${BUILD_BOARD}" -d "${BUILD_DIR}" "${BUILD_APP_DIR}" \
+        -- ${CMAKE_EXTRA_ARGS}
+
 fi
 
 if [ "${TWISTER}" = "true" ]; then
@@ -39,5 +42,30 @@ if [ "${TWISTER}" = "true" ]; then
     fi
     ./zephyr/scripts/twister -p "${TWISTER_BOARD}" ${TWISTER_ARGS} \
         -T "${TWISTER_APP_DIR}"
+fi
 
+# Signing manifest shall contain on each line path of the application to sign
+# and its version separated by a ';':
+# path/to/app; 1.0
+# path/to/app2; 1.1
+if [ "${SIGN}" = "true" ]; then
+    SIGN_KEY="${INPUT_SIGN_KEY:-bootloader/mcuboot/root-rsa-2048.pem}"
+    SIGN_ALIGNMENT="${INPUT_SIGN_ALIGNMENT:-32}"
+    SIGN_MANIFEST="${INPUT_SIGN_MANIFEST:?Missing signing manifest}"
+    if [ ! -f ${SIGN_MANIFEST} ]; then
+        echo "Error: INPUT_SIGN_MANIFEST: invalid file ${SIGN_MANIFEST}"
+        exit 1
+    fi
+    while read l; do
+        APP=$(echo ${l} | cut -d';' -f 1)
+        VERSION=$(echo ${l} | cut -d';' -f 2)
+        if [ ! -d "${APP}" ]; then
+            echo "Error: ${APP} invalid directory"
+            exit 1
+        fi
+        west --verbose sign -d ${APP} -t imgtool \
+            -p bootloader/mcuboot/scripts/imgtool.py \
+            -- --key ${SIGN_KEY} --align ${SIGN_ALIGNMENT} --pad \
+            --version ${VERSION}
+    done <${SIGN_MANIFEST}
 fi
